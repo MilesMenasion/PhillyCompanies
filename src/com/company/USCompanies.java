625b624a4007d55feabfca1042afbd3ad54a3732
ariemelvin <ariemelvinwork@gmail.com>
2022-05-06 13:06:18 -0400

    We combined our code together

---
+++ src/com/company/USCompanies.java
@@ -0,0 +1,13 @@
+package com.company;
+public class USCompanies {
+    //Fields
+    String name;
+    String industy;
+    double revenue;
+    float revenuegrowth;
+    String headquarters;
+    //Methods
+    void describe() {
+        System.out.println("This company is called " + name + ", and it's industy is " + industy + ".");
+    }
+}
